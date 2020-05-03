c55c12e93e4db4eb6dbae5811233bf013b5146f3
tanushree-sharma <tanushree@hotmail.ca>
2020-05-03 18:01:57 -0400

    first-commit

---
+++ futures.py
@@ -0,0 +1,17 @@
+# get stock future data from CNN
+# scrape the html using beautiful soup
+
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+#url = 'https://money.cnn.com/data/premarket/'
+#response = requests.get(url)
+
+#soup = BeautifulSoup(response.text, “html.parser”)
+
+#print(response)
+
+def main():
+    return "Down 10%"
